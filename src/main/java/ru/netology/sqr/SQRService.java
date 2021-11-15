a841d3ef4d73abb2e347bdfafd89c6828be42b26
Natalia Alferova <natalia@alferova.me>
2021-11-15 09:48:39 +0100

    Rename method, add test-cases

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -1,7 +1,7 @@
 package ru.netology.sqr;
 
 public class SQRService {
-    public int counter(int min, int max) {
+    public int countSquare (int min, int max) {
         int amount = 0;
         for (int i = 10; i <= 99; i++) {
             if (i * i >= min & i * i <= max) {
--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -14,13 +14,16 @@ class SQRServiceTest {
             "bottom=min and top>max, 200, 301, 3",
             "bottom=min and top<max, 200, 299, 3",
             "bottom>min and top<max, 201, 299, 3",
-            "bottom<min and top>max, 199, 301, 3"}
+            "bottom<min and top>max, 199, 301, 3",
+            "0 Square, 260, 280, 0",
+            "Square=min, 225, 300, 3",
+            "Square=max, 200, 289, 3"}
     )
 
-    void counter(String testName, int min, int max, int expected) {
+    void count(String testName, int min, int max, int expected) {
 
         SQRService service = new SQRService ();
-        int actual = service.counter(min, max);
+        int actual = service.countSquare(min, max);
 
         assertEquals(expected, actual);
     }
